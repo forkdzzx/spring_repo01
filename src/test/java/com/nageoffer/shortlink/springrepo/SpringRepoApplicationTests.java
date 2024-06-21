163db9cf73be82cc31190c0aa991ace7d8d5e1f5
David <754675468@qq.com>
2024-06-21 15:37:30 +0800

    init

---
+++ src/test/java/com/nageoffer/shortlink/springrepo/SpringRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nageoffer.shortlink.springrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
